098b4f6556f8dfbaf6b005f6ac117ba597cf60de
Jakub Godawa <jakub.godawa@gmail.com>
2018-10-19 15:47:49 +0200

    sleep between curls because of webrick limitations

--- curl.sh
+++ curl.sh
@@ -1,1 +1,5 @@
-curl -d '{"start_address":"Slowackiego 3, Otwock, Polska", "destination_address":"Plac Europejski 2, Warszawa, Polska", "price":"99.99", "date":"2018-10-17"}' -H "Content-Type: application/json" -X POST http://localhost:9292/api/trips
+curl -d '{"start_address":"Slowackiego 3, Otwock, Polska", "destination_address":"Plac Europejski 2, Warszawa, Polska", "price":"99.99", "date":"2018-10-16"}' -H "Content-Type: application/json" -X POST http://localhost:9292/api/trips
+sleep .2
+curl -d '{"start_address":"Muzeum Narodowe, Warszawa, Polska", "destination_address":"Plac Europejski 2, Warszawa, Polska", "price":"200", "date":"2018-10-15"}' -H "Content-Type: application/json" -X POST http://localhost:9292/api/trips
+sleep .2
+curl -d '{"start_address":"Teatr Nowy, Poznań, Polska", "destination_address":"Plac Europejski 2, Warszawa, Polska", "price":"10.00", "date":"2018-10-18"}' -H "Content-Type: application/json" -X POST http://localhost:9292/api/trips
