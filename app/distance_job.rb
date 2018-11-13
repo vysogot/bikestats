faba85d1bfda633bb0a06288e04f97957907a7e6
Jakub Godawa <jakub.godawa@gmail.com>
2018-11-13 16:26:00 +0100

    Fix #7 #8 abstract Open Route API client and pass params in a hash

--- app/distance_job.rb
+++ app/distance_job.rb
@@ -2,50 +2,10 @@ require 'active_job'
 require 'rest-client'
 
 class DistanceJob < ActiveJob::Base
-
-  OPENROUTE_API_KEY = ENV['OPENROUTE_API_KEY'] || ""
-  OPENROUTE_URI = 'https://api.openrouteservice.org/'
-  HEADERS = { :accept => 'application/json; charset=utf-8' }
-
   def perform(trip_id)
     trip = Trip.find(trip_id)
-
-    start = locate(trip.start_address)
-    destination = locate(trip.destination_address)
-    distance = fetch_distance(start, destination)
-
+    distance = OpenRouteClient.fetch_distance(trip.start_address, trip.destination_address)
     trip.update_attribute(:distance, distance)
   end
-
-  def locate(address)
-    response = RestClient.get(OPENROUTE_URI +
-                             'geocode/search?api_key=' + OPENROUTE_API_KEY +
-                             '&text=' + URI.encode(address),
-                             HEADERS)
-
-    json_body = JSON.parse(response.body)
-    coordinates = json_body["features"].first["geometry"]["coordinates"]
-
-    return {
-      latitude: coordinates[0],
-      longitude: coordinates[1]
-    }
-  end
-
-  def fetch_distance(start, destination)
-    coordinates = start[:latitude].to_s + "," + start[:longitude].to_s + "|" +
-                  destination[:latitude].to_s + "," + destination[:longitude].to_s
-
-    response = RestClient.get(OPENROUTE_URI +
-                              'directions?api_key=' + OPENROUTE_API_KEY +
-                              '&coordinates=' + URI.encode(coordinates) +
-                              '&profile=cycling-road',
-                              HEADERS)
-
-    json_body = JSON.parse(response.body)
-    distance_in_meters = json_body["routes"].first["summary"]["distance"]
-    distance_in_kilometers = (distance_in_meters/1000).round(0)
-
-    return distance_in_kilometers
-  end
 end
+
